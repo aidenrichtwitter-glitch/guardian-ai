c10a3f389086617769fa12012b283309636d3be6
aidenrichtwitter-glitch <aidenrichtwitter@gmail.com>
2026-03-14 05:00:03 +0000

    Remove invalid .next build folder

--- projects/next-js-boilerplate/.next/dev/server/chunks/ssr/71424__next-internal_server_app_[locale]_(marketing)_page_actions_9df154d2.js
+++
@@ -1,9 +0,0 @@
-;!function(){try { var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof global?global:"undefined"!=typeof window?window:"undefined"!=typeof self?self:{},n=(new e.Error).stack;n&&((e._debugIds|| (e._debugIds={}))[n]="e4cd3433-832e-7ac2-51de-93a6228b17ce")}catch(e){}}();
-module.exports = [
-"[project]/projects/next-js-boilerplate/.next-internal/server/app/[locale]/(marketing)/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# debugId=e4cd3433-832e-7ac2-51de-93a6228b17ce
-//# sourceMappingURL=71424__next-internal_server_app_%5Blocale%5D_%28marketing%29_page_actions_9df154d2.js.map
--- projects/next-js-boilerplate/.next/dev/static/chunks/projects_next-js-boilerplate_src_app_[locale]_(marketing)_page_tsx_2d2de107._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/b5a40_next_dist_e617f09d._.js"
-],
-    source: "dynamic"
-});
